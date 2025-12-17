93b211829016ab706a3f243b558ffbe05accb882
Kevin Rama <147321350+kevinrama05@users.noreply.github.com>
2025-12-17 13:03:00 +0000

    Simple Sorting

--- ps_smart_rotate.c
+++ ps_smart_rotate.c
@@ -12,21 +12,58 @@
 
 #include "push_swap.h"
 
-static int	ft_find_element(t_stack *tmp, int min, int max)
+static int	ft_find_element_a(t_ps_data *d, int min, int max)
 {
-	int	i;
+	t_node	*head_temp;
+	t_node	*tail_temp;
+	int		dist_head;
+	int		dist_tail;
 
-	i = 1;
-	while (i < tmp->size / 2)
+	head_temp = d->a->head;
+	tail_temp = d->a->tail;
+	dist_head = 1;
+	dist_tail = -1;
+	while (head_temp)
 	{
-		if (tmp->head->index >= min && tmp->head->index <= max)
-			return (0);
-		else
-			tmp->head = tmp->head->next;
-		i++;
+		if (head_temp->index >= min && head_temp->index <= max)
+			break ;
+		if (tail_temp->index >= min && head_temp->index <= max)
+			break ;
+		dist_head++;
+		dist_tail--;
+		head_temp = head_temp->next;
+		tail_temp = tail_temp->prev;
 	}
-	return (1);
-	
+	if (-dist_tail < dist_head)
+		return (dist_tail);
+	return (dist_head);
+}
+
+static int	ft_find_element_b(t_ps_data *d, int min, int max)
+{
+	t_node	*head_temp;
+	t_node	*tail_temp;
+	int		dist_head;
+	int		dist_tail;
+
+	head_temp = d->b->head;
+	tail_temp = d->b->tail;
+	dist_head = 1;
+	dist_tail = -1;
+	while (head_temp)
+	{
+		if (head_temp->index >= min && head_temp->index <= max)
+			break ;
+		if (tail_temp->index >= min && head_temp->index <= max)
+			break ;
+		dist_head++;
+		dist_tail--;
+		head_temp = head_temp->next;
+		tail_temp = tail_temp->prev;
+	}
+	if (-dist_tail < dist_head)
+		return (dist_tail);
+	return (dist_head);
 }
 
 void	ft_smart_rotate_b(t_ps_data *data, int min, int max)
@@ -40,15 +77,15 @@ void	ft_smart_rotate_b(t_ps_data *data, int min, int max)
 		sb(data);
 	else
 	{
-		i = ft_find_element(data->b, min, max);
+		i = ft_find_element_b(data, min, max);
 		if (i == 0)
 		{
-			while (!(data->b->head->index >= min && data->b->head->index <= max))
+			while (data->b->head->index > max || data->b->head->index < min)
 				rb(data);
 		}
 		else
 		{
-			while (!(data->b->head->index >= min && data->b->head->index <= max))
+			while (data->b->head->index < min || data->b->head->index > max)
 				rrb(data);
 		}
 	}
@@ -65,15 +102,15 @@ void	ft_smart_rotate_a(t_ps_data *data, int min, int max)
 		sa(data);
 	else
 	{
-		i = ft_find_element(data->a, min, max);
-		if (i == 0)
+		i = ft_find_element_a(data, min, max);
+		if (i > 0)
 		{
-			while (!(data->a->head->index >= min && data->a->head->index <= max))
+			while (data->a->head->index > max || data->a->head->index < min)
 				ra(data);
 		}
 		else
 		{
-			while (!(data->a->head->index >= min && data->a->head->index <= max))
+			while (data->a->head->index > max || data->a->head->index < min)
 				rra(data);
 		}
 	}
